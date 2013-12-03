8a7befcefb3cfbb0891fdc5da2b5b3ca579c8b07
spraveenitpro <spraveenitpro@gmail.com>
2013-12-03 15:51:36 +0530

    Rebuilding ThemeShaper for Best practice

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<?php get_header(); ?>
+
+
+<div id="primary" class="content-area">
+    <div id="content" class="site-content" role="main">
+    </div><!-- #content .site-content -->
+</div><!-- #primary .content-area -->
+
+<?php get_footer(); ?>
