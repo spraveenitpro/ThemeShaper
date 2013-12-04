4253c002a418ae7b06f0479ca80dbe70660282f3
spraveenitpro <spraveenitpro@gmail.com>
2013-12-04 16:34:35 +0530

    Completed Header

--- functions.php
+++ functions.php
@@ -31,12 +31,12 @@ if (! function_exists('shape_setup') ):
 		/**
      * Custom template tags for this theme.
      */
-    require( get_template_directory() . '/inc/template-tags.php' );
+        require( get_template_directory() . '/inc/template-tags.php' );
  
     /**
      * Custom functions that act independently of the theme templates
      */
-    require( get_template_directory() . '/inc/tweaks.php' );
+        require( get_template_directory() . '/inc/tweaks.php' );
  
     /**
      * Make theme available for translation
@@ -44,28 +44,49 @@ if (! function_exists('shape_setup') ):
      * If you're building a theme based on Shape, use a find and replace
      * to change 'shape' to the name of your theme in all the template files
      */
-    load_theme_textdomain( 'shape', get_template_directory() . '/languages' );
+        load_theme_textdomain( 'shape', get_template_directory() . '/languages' );
  
     /**
      * Add default posts and comments RSS feed links to head
      */
-    add_theme_support( 'automatic-feed-links' );
+        add_theme_support( 'automatic-feed-links' );
  
     /**
      * Enable support for the Aside Post Format
      */
-    add_theme_support( 'post-formats', array( 'aside' ) );
+        add_theme_support( 'post-formats', array( 'aside' ) );
  
     /**
      * This theme uses wp_nav_menu() in one location.
-     */
-    register_nav_menus( array(
-        'primary' => __( 'Primary Menu', 'shape' ),
-    ) );
+         */
+        register_nav_menus( array(
+            'primary' => __( 'Primary Menu', 'shape' ),
+        ) );
 
 
 	}
 	endif; //shape_setup
 
 	add_action('after_setup_theme','shape_setup');
+
+
+
+    /**
+ * Enqueue scripts and styles
+ */
+function shape_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+ 
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+ 
+    wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+ 
+    if ( is_singular() && wp_attachment_is_image() ) {
+        wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'shape_scripts' );
+
 
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-
 <?php get_header(); ?>
 
 
 <div id="primary" class="content-area">
-    <div id="content" class="site-content" role="main">
+    <div id="content" class="site-content" role="main">bla bla
     </div><!-- #content .site-content -->
 </div><!-- #primary .content-area -->
 
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
